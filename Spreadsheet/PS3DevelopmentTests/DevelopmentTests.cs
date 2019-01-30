ec4d794f0ab243d0c25bd207d2a13c8d18874bb9
RYAN HAWKINS <u1105702@umail.utah.edu>
2019-01-29 18:53:58 -0700

    Created stress tests for the replace methods to see if they were efficient enough. Yet again, more tests tofollow.

--- Spreadsheet/PS3DevelopmentTests/DevelopmentTests.cs
+++ Spreadsheet/PS3DevelopmentTests/DevelopmentTests.cs
@@ -1,7 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Dependencies;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Dependencies;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
 
 namespace DevelopmentTests
 {
@@ -216,27 +218,174 @@ namespace DevelopmentTests
             Assert.IsTrue(t.HasDependents("b"));
             Assert.IsTrue(t.HasDependees("b"));
         }
-
+        /// <summary>
+        /// Ensures that the ReplaceDependees method performs as specified.
+        /// Specifically checks to see if size is updating correctly and that
+        /// the dependencies are being correctly removed.
+        /// </summary>
         [TestMethod]
         public void TestReplaceDependeesSizeChange()
         {
             DependencyGraph t = new DependencyGraph();
             t.AddDependency("x", "y");
+            t.AddDependency("t", "y");
+            t.AddDependency("r", "y");
+            t.AddDependency("e", "y");
+            t.AddDependency("w", "y");
+            t.AddDependency("q", "y");
+            t.AddDependency("a", "y");
+            t.AddDependency("z", "y");
             t.ReplaceDependees("y", new HashSet<string>());
             Assert.AreEqual(0, t.Size);
             Assert.IsFalse(t.HasDependents("x"));
+            Assert.IsFalse(t.HasDependents("t"));
+            Assert.IsFalse(t.HasDependents("r"));
+            Assert.IsFalse(t.HasDependents("e"));
+            Assert.IsFalse(t.HasDependents("w"));
+            Assert.IsFalse(t.HasDependents("q"));
+            Assert.IsFalse(t.HasDependents("a"));
+            Assert.IsFalse(t.HasDependents("z"));
             Assert.IsFalse(t.HasDependees("y"));
         }
-
+        /// <summary>
+        /// 
+        /// </summary>
         [TestMethod]
         public void TestReplaceDependentsSizeChange()
         {
             DependencyGraph t = new DependencyGraph();
             t.AddDependency("x", "y");
+            t.AddDependency("x", "t");
+            t.AddDependency("x", "e");
+            t.AddDependency("x", "r");
+            t.AddDependency("x", "w");
+            t.AddDependency("x", "q");
+            t.AddDependency("x", "a");
+            t.AddDependency("x", "s");
+            t.AddDependency("x", "d");
+            t.AddDependency("x", "f");
+            t.AddDependency("x", "g");
+
             t.ReplaceDependents("x", new HashSet<string>());
             Assert.AreEqual(0, t.Size);
             Assert.IsFalse(t.HasDependents("x"));
             Assert.IsFalse(t.HasDependees("y"));
+            Assert.IsFalse(t.HasDependees("t"));
+            Assert.IsFalse(t.HasDependees("e"));
+            Assert.IsFalse(t.HasDependees("r"));
+            Assert.IsFalse(t.HasDependees("w"));
+            Assert.IsFalse(t.HasDependees("q"));
+            Assert.IsFalse(t.HasDependees("a"));
+            Assert.IsFalse(t.HasDependees("s"));
+            Assert.IsFalse(t.HasDependees("d"));
+            Assert.IsFalse(t.HasDependees("f"));
+            Assert.IsFalse(t.HasDependees("g"));
+
+        }
+        /// <summary>
+        /// Stress tests the AddDependency method by adding 1,000,000 
+        /// dependencies, all with the same dependent.
+        /// </summary>
+        [TestMethod]
+        public void StressTestAddDependency()
+        {
+            DependencyGraph t = new DependencyGraph();
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            for (int i = 0; i < 1_000_000; i++)
+            {
+
+                t.AddDependency("s", RandomStringGenerator(15, 7));
+            }
+            if (sw.ElapsedMilliseconds >= 10_000)
+            {
+                Assert.Fail();
+            }
+        }
+        /// <summary>
+        /// Stress tests the ReplaceDependents method by adding 1,000,000
+        /// dependencies to the DependencyGraph and then replacing all of them
+        /// with a million different dependencies.
+        /// </summary>
+        [TestMethod]
+        public void StressTestReplaceDependents()
+        {
+            DependencyGraph t = new DependencyGraph();
+
+            Stopwatch sw = new Stopwatch();
+            for (int i = 0; i < 1_000_000; i++)
+            {
+
+                t.AddDependency("s", RandomStringGenerator(15, 7));
+            }
+
+            List<string> ls = new List<string>(1_000_000);
+            for (int i = 0; i < 1_000_000; i++)
+            {
+                ls.Add(RandomStringGenerator(15, 7));
+            }
+
+            sw.Start();
+            t.ReplaceDependents("s", ls);
+
+            if (sw.ElapsedMilliseconds >= 10_000)
+            {
+                Assert.Fail();
+            }
+        }
+
+        /// <summary>
+        /// Stress tests the ReplaceDependees method by adding 1,000,000
+        /// dependencies to the DependencyGraph and then replacing all of them
+        /// with a million different dependencies.
+        /// </summary>
+        [TestMethod]
+        public void StressTestReplaceDependees()
+        {
+            DependencyGraph t = new DependencyGraph();
+
+            Stopwatch sw = new Stopwatch();
+            for (int i = 0; i < 1_000_000; i++)
+            {
+
+                t.AddDependency(RandomStringGenerator(15, 7), "s");
+            }
+
+            List<string> ls = new List<string>(1_000_000);
+            for (int i = 0; i < 1_000_000; i++)
+            {
+                ls.Add(RandomStringGenerator(15, 7));
+            }
+
+            sw.Start();
+            t.ReplaceDependees("s", ls);
+
+            if (sw.ElapsedMilliseconds >= 10_000)
+            {
+                Assert.Fail();
+            }
+        }
+        /// <summary>
+        /// Generates random strings for stress testing the DependencyGraph 
+        /// class
+        /// </summary>
+        /// <param name="length">
+        /// The desired length of the random string.
+        /// </param>
+        /// <returns>
+        /// A random string made of lowercase English letters.
+        /// </returns>
+        private static string RandomStringGenerator(int length, int seed)
+        {
+            StringBuilder str = new StringBuilder();
+            Random r = new Random(seed);
+            for (int i = 0; i < length; i++)
+            {
+                str.Append((char)r.Next(97, 123));
+            }
+
+            return str.ToString();
         }
     }
 }
