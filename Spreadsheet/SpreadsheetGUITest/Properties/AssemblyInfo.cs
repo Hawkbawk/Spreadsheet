124442e0c731399734aec5b14671cb856a87933b
CURTIS LIN <u1116418@umail.utah.edu>
2019-03-07 15:50:12 -0700

    CREATED THE GUI UNIT TESTS

---
+++ Spreadsheet/SpreadsheetGUITest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SpreadsheetGUITest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("College of Engineering")]
+[assembly: AssemblyProduct("SpreadsheetGUITest")]
+[assembly: AssemblyCopyright("Copyright © College of Engineering 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("365a749d-d803-4411-86c7-6b1bffa22286")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Spreadsheet/SpreadsheetGUITest/SpreadsheetTestStub.cs
@@ -0,0 +1,60 @@
+﻿using SpreadsheetGUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpreadsheetGUITest
+{
+    class SpreadsheetTestStub : ISpreadsheetView
+    {
+        public event Action ChangeContents;
+        public event Action CloseEvent;
+        public event Action NewCellSelected;
+        public event Action NewEvent;
+        public event Action<string> OpenEvent;
+        public event Action<string> SaveEvent;
+
+        public void BeginCloseWithoutSave()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DoClose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetDesiredContents()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetSelection(out int col, out int row)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OpenNew()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OpenNew(string filename)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SelectedNewCell(string contents)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetValue(int col, int row, string content)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
