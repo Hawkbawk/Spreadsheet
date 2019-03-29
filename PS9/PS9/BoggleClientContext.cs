2636eb1e0449142e8e49eeb4290886e870f3fca6
Ryan Hawkins <u1105702@umail.utah.edu>
2019-03-28 18:16:18 -0600

    Added in the boilerplate code necessary to implement MVC style GUI building. Good luck my dudes.

---
+++ PS9/PS9/BoggleClientContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PS9
+{
+    class BoggleClientContext : ApplicationContext
+    {
+        private static BoggleClientContext context;
+
+        private BoggleClientContext()
+        {
+
+        }
+        public static BoggleClientContext GetContext()
+        {
+            if (context == null)
+            {
+                context = new BoggleClientContext();
+            }
+            return context;
+        }
+
+        public void RunNew()
+        {
+            BoggleClient view = new BoggleClient();
+            new BoggleClientController(view);
+
+            view.Show();
+        }
+
+    }
+}
---
+++ PS9/PS9/BoggleClientController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS9
+{
+    class BoggleClientController
+    {
+        IBoggleService view;
+
+        public BoggleClientController(IBoggleService _view)
+        {
+            view = _view;
+
+            view.EnterGame += HandleEnterGame;
+            view.CancelGame += HandleCancelGame;
+            view.SubmitWord += HandleSubmitWord;
+
+        }
+
+        private void HandleSubmitWord(string obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void HandleCancelGame()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void HandleEnterGame()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
